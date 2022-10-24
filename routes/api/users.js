0fd00ec3dc992989d8dc7e11f621922dcfdd8398
HopeAero <davidsalcedo388@gmail.com>
2022-10-24 01:02:58 -0430

    modificaciones

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,30 +2,30 @@ const router = require('express').Router();
  
 const { User }= require('../../dataBase');
 
-router.get('/', async (req,res)=>{
+router.get('/users', async (req,res)=>{
     const users = await User.findAll();
     res.json(users);
 });
 
 //endpoint para crear usuarios
-router.post('/', async (req,res)=>{
+router.post('/users', async (req,res)=>{
     const user = await User.create(req.body);
     res.json(user);
 });
 
 //endpoint para editar usuarios
-router.put('/:userId', async (req,res)=>{
+router.put('/users/:userId', async (req,res)=>{
     await User.update(req.body,{
         where:{ id: req.params.userId}
     });
-    res.json({ success:'Se ha modificado'});
+    res.json({ success: true, message:'Se ha modificado'});
 });
 
-router.delete('/:userId', async (req,res)=>{
+router.delete('/users/:userId', async (req,res)=>{
     await User.destroy({
         where:{ id: req.params.userId}
     });
-    res.json({ success:'Se ha eliminado'});
+    res.json({ success: true, message:'Se ha eliminado'});
 });
 
 module.exports = router;
