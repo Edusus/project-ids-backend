4ab239cec0698ca32b1e5f49488b4e1056db401e
HopeAero <davidsalcedo388@gmail.com>
2022-10-24 23:16:42 -0430

    paginacion

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,26 +2,35 @@ const router = require('express').Router();
  
 const { User }= require('../../dataBase');
 
-router.get('/users', async (req,res)=>{
-    const users = await User.findAll();
+router.get('/', async (req,res)=>{
+
+    const {page = 0, size = 10} = req.query;
+
+    let options = {
+        limit: +size,
+        offset: (+page) * (+size)
+    };
+
+    const users = await User.findAll(options);
+
     res.json(users);
 });
 
 //endpoint para crear usuarios
-router.post('/users', async (req,res)=>{
+router.post('/', async (req,res)=>{
     const user = await User.create(req.body);
     res.json(user);
 });
 
 //endpoint para editar usuarios
-router.put('/users/:userId', async (req,res)=>{
+router.put('/:userId', async (req,res)=>{
     await User.update(req.body,{
         where:{ id: req.params.userId}
     });
     res.json({ success: true, message:'Se ha modificado'});
 });
 
-router.delete('/users/:userId', async (req,res)=>{
+router.delete('/:userId', async (req,res)=>{
     await User.destroy({
         where:{ id: req.params.userId}
     });
