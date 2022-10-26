ce0b6b5b2df9519ace76b0c75d593e5496d0d80b
NØRA <joseandresrs0407@gmail.com>
2022-10-25 21:42:19 -0400

    added cors

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 require('dotenv').config();
 
 const express = require('express');
+const cors = require('cors');
 const routes = require('./routes.js');
 
 const app = express();
 
 const PORT = process.env.PORT || 8080;
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/api', routes);
