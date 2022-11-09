f65eac58b6c217fbfb7695ef7a14debe269790f6
Daikone <eduardoarzolay@gmail.com>
2022-11-09 03:02:24 -0400

    Refactorizada la paginación y añadida la filtración por parámetros query en una misma ruta (/ads/search).

--- routes/api/ads.js
+++ routes/api/ads.js
@@ -2,7 +2,8 @@ const { Router } = require('express');
 const { Op } = require('sequelize');
 const { ad } = require('../../databases/db');
 
-const ads = Router();
+const adsRouter = Router();
+const allowedFields = ['announcer', 'adType', 'redirecTo', 'img'];
 
 const httpGetResponse = (res, resource, resourceName) => {
   if (resource) {
@@ -16,57 +17,56 @@ const findAdById = (id) => {
   return ad.findByPk(id);
 }
 
-ads.get('/', async (req, res) => {
+adsRouter.get('/', async (req, res) => {
   const ads = await ad.findAll();
   httpGetResponse(res, ads, 'ads');
 });
 
-// TODO: Refactorizar paginación
-ads.get('/paginated', async (req, res) => {
+adsRouter.get('/search', async (req, res) => {
   try {
-    let previousId = ad.findOne().id;
+    let { page = 0, size = 10, announcer: ann = '.*', adtype: type = ['static', 'float'] } = req.query;
+    const [ pageAsNumber, sizeAsNumber ] = [ Number.parseInt(page), Number.parseInt(size) ];
     let options = {
+      limit: sizeAsNumber,
+      offset: pageAsNumber * sizeAsNumber, 
       where: {
-        id: {
-          [Op.gt]: previousId
-        }
+        announcer: {
+          [Op.regexp]: ann
+        },
+        adtype: type
       },
-      limit: 10
     };
-    do {
-      const ads = await ad.findAndCountAll(options);
-      previousId++;
-      httpGetResponse(res, ads);
-      console.log(ads.count, ads.rows.length);
-    } while (previousId < ads.count * ads.rows.length)
+    const { count, rows } = await ad.findAndCountAll(options);
+    httpGetResponse(res, {
+      totalAds: Math.min(count, sizeAsNumber, rows.length),
+      pageNumber: pageAsNumber,
+      pageSize: sizeAsNumber,
+      ads: rows
+    }, 'ads');
   } catch (error) {
     console.error(error);
+    res.send(error.message);
   }
 });
 
-ads.get('/:adId', async (req, res) => {
+adsRouter.get('/:adId', async (req, res) => {
   const reqAd = await findAdById(req.params.adId);
   httpGetResponse(res, reqAd, 'Required ad');
 });
 
-ads.post('/', async (req, res) => {
+adsRouter.post('/', async (req, res) => {
   try {
-    const upAd = await ad.create(req.body, { fields: [
-      'announcer', 
-      'adType', 
-      'redirectTo', 
-      'img'
-    ]});
+    const upAd = await ad.create(req.body, { fields: allowedFields});
     res.status(201).json(upAd);
   } catch (error) {
-    console.error(error.toJSON());
+    console.error(error);
     res.status(400).send(error.message);
   }
 });
 
-ads.put('/:adId', async (req, res) => {
+adsRouter.put('/:adId', async (req, res) => {
   if (await findAdById(req.params.adId)) {
-    await ad.update(req.body, { 
+    await ad.update(req.body, { fields: allowedFields,
       where: { id: req.params.adId }
     });
     res.status(200).send('Modified ad ' + req.params.adId);
@@ -75,7 +75,7 @@ ads.put('/:adId', async (req, res) => {
   }
 });
 
-ads.delete('/:adId', async (req, res) => {
+adsRouter.delete('/:adId', async (req, res) => {
   if (await findAdById(req.params.adId)) {
     await ad.destroy({
       where: { id: req.params.adId }
@@ -86,4 +86,4 @@ ads.delete('/:adId', async (req, res) => {
   }
 });
 
-module.exports = ads;
+module.exports = adsRouter;
