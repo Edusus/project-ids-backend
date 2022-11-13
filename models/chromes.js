65359b1d255c53152c1ca8d54d04dfc48ec6c59b
HopeAero <davidsalcedo388@gmail.com>
2022-11-12 21:25:36 -0430

    correcion de ID de cromos

--- models/chromes.js
+++ models/chromes.js
@@ -3,7 +3,8 @@ module.exports=(sequelize,type)=>{
         id:{
             type:type.INTEGER,
             primaryKey:true,
-            autoIncrement:true
+            autoIncrement:true,
+            allowNull:false
         },
         playerName:{
             type:type.STRING,
