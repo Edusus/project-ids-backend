69870aa623354eb4f0e17a6da84c79f46382f400
HopeAero <davidsalcedo388@gmail.com>
2022-12-21 17:09:25 -0430

    furulando

--- controllers/benches/finder.js
+++ controllers/benches/finder.js
@@ -4,7 +4,7 @@ const responses = require('../../utils/responses/responses');
 
 const find = async (req, res) => {
   const userId = req.user.id.id;
-  const eventId = req.eventId;
+  const eventId = req.params.eventId
   const warehouses = await Warehouse.findAndCountAll({
     where: {
       userId,
