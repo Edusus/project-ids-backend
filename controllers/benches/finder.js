5004da07ed7e29d2a0a45e2e0516703ae22b6321
Daikone <eduardoarzolay@gmail.com>
2022-12-23 17:24:52 -0400

    Añadidos los filtros a la banca por nombre del jugador, por posicion del jugador y por nombre del equipo

--- controllers/benches/finder.js
+++ controllers/benches/finder.js
@@ -1,46 +1,63 @@
 const { Op } = require('sequelize');
 const { Warehouse, Sticker, team } = require('../../databases/db');
+const sticker = require('../../models/sticker');
+const warehouses = require('../../models/warehouses');
 const responses = require('../../utils/responses/responses');
 
+
+/**
+ * Find receives a page number and a page size. It gets the user's paginated bench from their warehouse 
+ * and returns the paginated result.
+ * @param req - the request object
+ * @param res - the response object
+ */
 const find = async (req, res) => {
-  const userId = req.user.id.id;
-  const eventId = req.eventId;
-  const warehouses = await Warehouse.findAndCountAll({
-    where: {
-      userId,
-      eventId
-    }
-  });
-  const { count, rows } = warehouses;
-
-  let [stickers, teams, items] = [[], [], []];
-  const itemsLength = rows.length;
-  for (let i = 0; i < itemsLength; i++) {
-    stickers.push(await rows[i].getSticker({
-      attributes: ['id', 'playerName', 'position', 'img', 'createdAt', 'updatedAt', 'teamId']
-    }));
-
-    teams.push(await stickers[i].getTeam({
-      attributes: ['id', 'name', 'badge']
-    }));
-
-    let item = {
-      id: stickers[i].id,
-      playerName: stickers[i].playerName,
-      position: stickers[i].position,
-      img: stickers[i].img,
-      createdAt: stickers[i].createdAt,
-      updatedAt: stickers[i].updatedAt,
-      team: {
-        id: teams[i].id,
-        name: teams[i].name,
-        badge: teams[i].badge
+  try {
+    let { page = 0, size = 10, playername: playerName = '.*', teamname = '%', position = ['arquero', 'defensa', 'delantero', 'medio campo'] } = req.query;
+    const [ pageAsNumber, sizeAsNumber ] = [ Number.parseInt(page), Number.parseInt(size) ];
+    const userId = req.user.id.id;
+    const eventId = req.eventId;
+    let options = {
+      limit: sizeAsNumber,
+      offset: pageAsNumber * sizeAsNumber,
+      where: {
+        userId,
+        eventId,
+        isInLineup: false
+      },
+      include: {
+        model: Sticker,
+        attributes: ['id', 'playerName', 'position', 'img', 'createdAt', 'updatedAt'],
+        where: {
+          playerName: {
+            [Op.regexp]: playerName
+          },
+          position
+        },
+        include: {
+          model: team,
+          attributes: ['id', 'name', 'badge'],
+          where: {
+            name: {
+              [Op.like]: teamname
+            }
+          }
+        }
       }
     }
-    items.push(item);
-  }
+    const { count, rows } = await Warehouse.findAndCountAll(options);
 
-  responses.paginatedDTOsResponse(res, 200, 'Almacen recuperado con exito', items, count, 0, 100);
+    const warehouses = JSON.parse(JSON.stringify(rows));
+    const items = [];
+    for (let i = 0; i < warehouses.length; i++) {
+      items.push(warehouses[i].sticker);
+    }
+
+    responses.paginatedDTOsResponse(res, 200, 'Almacen recuperado con exito', items, count, pageAsNumber, sizeAsNumber);
+  } catch(err) {
+    console.log(err);
+    responses.errorDTOResponse(res, 400, err.message);
+  }
 }
 
 const finder = {
--- routes/api/bench.js
+++ routes/api/bench.js
@@ -1,9 +1,18 @@
 const router = require('express').Router();
 const { finder } = require('../../controllers/benchesControllers');
 
+/**
+ * Route to get a page of benches, can be filtered by a like of the team name,
+ * a regexp of the player name and by the position of the players.
+ * * Query params:
+ * @param page: an integer representing the page to get, default value is 0
+ * @param size: an integer representing the size of each page, default value is 10
+ * @param teamname: a like representing the filter to the teams name, default value is % (any team name)
+ * @param playername: a regexp representing the filter to the player's name, default value is .* (any player)
+ * @param position: represents the filter to the position of the players, 
+ * default value is ['arquero', 'defensa', 'delantero', 'medio campo'] (any position)
+ */
+
 router.get('/', finder.find);
-
-
-router.delete('/:playerId', );
 
 module.exports = router;
