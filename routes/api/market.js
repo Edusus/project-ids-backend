76fbc69d7f61e577649390250991c95c0d1684b2
Edusus <ejsucre.19@est.ucab.edu.ve>
2023-01-06 20:42:16 -0400

    Endpoint para buscar subasta específica

--- routes/api/market.js
+++ routes/api/market.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const { poster, posterBid } = require('../../controllers/market/poster');
 const { bidUpdate } = require('../../controllers/market/updater');
-const { Market } = require('../../databases/db');
+const { Market,Bid,Op } = require('../../databases/db');
 
 router.get('/actives', async(req,res)=>{
     const marketActives= await Market.findAll({where:{ "isFinished":false}});
@@ -14,6 +14,53 @@ router.get('/actives', async(req,res)=>{
         });
     }
 });
+
+//endpoint para buscar subastas por su id
+router.get('/:marketId', async (req,res)=>{
+    const market= await Market.findOne({where:{id: req.params.marketId}});
+     if(!market){
+        return res.json({
+          success: false,
+          error:'No existe esta subasta'
+        });
+    }
+    
+    //Para saber la puja más alta de la subasta  
+    const marketBid = await Bid.findAll({
+        raw:true,
+        where:{marketId:req.params.marketId}
+    });
+    const user = JSON.parse(JSON.stringify(marketBid));
+    const items = [];
+    for (let i = 0; i < user.length; i++) {
+        items.push(user[i]);
+    }
+    const max = Math.max.apply(Math, items.map(function(o) { return o.value; }))
+    const winner = items.find(item => item.value === max);
+
+    //Para saber la última puja del usuario
+    const userBid = await Bid.findOne({
+        raw:true,
+        where: {
+            [Op.and]:[{
+                marketId : req.params.marketId
+            },
+            {
+                userId : req.user.id.id
+            }]
+        }
+    });
+    
+    return res.status(200).json({
+        success: true,
+        message:"Subasta recuperada con éxito",
+        item:{
+            market,
+            bidHigher:winner,
+            bidUser:userBid
+        }
+    });
+  });
 
 router.post('/add', poster);
 
