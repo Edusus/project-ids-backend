d70a59f8a2c2b1a21228ee0e61bb475aff673cfa
HopeAero <davidsalcedo388@gmail.com>
2023-01-09 21:34:56 -0400

    Bugs de respuesta pero furula

--- routes/api/market.js
+++ routes/api/market.js
@@ -1,41 +1,106 @@
 const router = require('express').Router();
-const { parse, stringify } = require('uuid');
 const { poster, posterBid } = require('../../controllers/market/poster');
 const { bidUpdate } = require('../../controllers/market/updater');
-const { Market,Bid,Op, Sticker, Team, User, Event } = require('../../databases/db');
+const { Market,Bid,Op, Sticker, Team, User, Event, PlayerFantasy } = require('../../databases/db');
 const responses = require('../../utils/responses/responses');
 
 router.get('/', async(req,res)=>{
-    let {page = 0, size = 10} = req.query;
+    let {page = 0, size = 10, myAuction = false} = req.query;
     const [ pageAsNumber, sizeAsNumber ] = [ Number.parseInt(page), Number.parseInt(size) ];
-    let options = {
-        limit: sizeAsNumber,
-        offset: pageAsNumber * sizeAsNumber,
-        include: {
-            model: Sticker,
+     if (myAuction === 'true') {
+        let options = {
+            limit: sizeAsNumber,
+            offset: pageAsNumber * sizeAsNumber,
             include: {
-                model: Team
+                model: Sticker,
+                include: {
+                    model: Team
+                }
+            },
+            order: [
+                ['createdAt', 'DESC']
+            ],
+            where: {
+                isFinished: false,
+                userId: req.user.id.id
             }
-        },
-        order: [
-            ['createdAt', 'DESC']
-        ],
-        where: {
-            isFinished: false,
-            userId: {
-                [Op.not]: Number.parseInt(req.user.id.id)
-            }
-        }
-    };
+        };
+        const { count, rows } = await Market.findAndCountAll(options);
+        return responses.paginatedDTOsResponse(res, 200, 'Subastas recuperadas con exito', rows, count, pageAsNumber, sizeAsNumber);
 
-    const { count, rows } = await Market.findAndCountAll(options);
-    return responses.paginatedDTOsResponse(res, 200, 'Subastas recuperadas con exito', rows, count, pageAsNumber, sizeAsNumber);
+     } else {    
+        let options = {
+            limit: sizeAsNumber,
+            offset: pageAsNumber * sizeAsNumber,
+            include: {
+                model: Sticker,
+                include: {
+                    model: Team
+                }
+            },
+            order: [
+                ['createdAt', 'DESC']
+            ],
+            where: {
+                isFinished: false,
+                userId: {
+                    [Op.not]: Number.parseInt(req.user.id.id)
+                }
+            }
+        };
+     
+        const { count, rows } = await Market.findAndCountAll(options);
+        return responses.paginatedDTOsResponse(res, 200, 'Subastas recuperadas con exito', rows, count, pageAsNumber, sizeAsNumber);
+    }
+    
 });
 
-//endpoint para buscar subastas por su id
-router.get('/:marketId', async (req,res) =>{
-    const market= await Market.findOne({
-        raws: true,
+router.get('/myBids', async(req,res) =>{ 
+    let {page = 0, size = 10} = req.query;
+    const [ pageAsNumber, sizeAsNumber ] = [ Number.parseInt(page), Number.parseInt(size) ];
+     let options = {
+         limit: sizeAsNumber,
+         offset: pageAsNumber * sizeAsNumber,
+         order: [
+             ['createdAt', 'DESC']
+         ],
+         where: {
+             userId: req.user.id.id
+         },
+         include : [{
+             model: User,
+             attributes: ['id', 'name']
+         },
+         {
+             model: Market,
+             attributes: {
+                 exclude: ['undefined']
+             },
+             where: {
+                 isFinished: false
+             }
+         }]
+     };
+ 
+     const { count, rows } = await Bid.findAndCountAll(options);
+ 
+     if(!rows){
+         return responses.errorDTOResponse(res, 404, 'No tienes pujas');
+     }
+ 
+     const bid = JSON.parse(JSON.stringify(rows));
+     const items = [];
+     for (let i = 0; i < bid.length; i++) {
+         items.push(bid[i]);
+     }
+ 
+    return responses.paginatedDTOsResponse(res, 200, 'Ofertas recuperadas con exito', items, count, pageAsNumber, sizeAsNumber);
+});
+
+// Endpoint para buscar subastas por su ID
+router.get('/:auctionId', async (req,res) => {
+    const market = await Market.findOne({
+        raw: true,
         include: [
             {
                 model: Sticker,
@@ -50,51 +115,49 @@ router.get('/:marketId', async (req,res) =>{
                 attributes: ['id', 'eventName']
             }
         ],
-        where:{id: req.params.marketId}
+        where:{id: req.params.auctionId}
     });
-     if(!market){
+
+    if  (!market) {
         return responses.errorDTOResponse(res, 404, 'Subasta no encontrada');
     }
-    
-    //Para saber la puja más alta de la subasta  
-    const marketBid = await Bid.findAll({ 
-        raw:true, 
-        where: { marketId: req.params.marketId },
-        include: {
-            model: User,
-            attributes: ['id', 'name']
-        } 
-    });
 
-    const user = JSON.parse(JSON.stringify(marketBid));
-    const items = [];
-    for (let i = 0; i < user.length; i++) {
-        items.push(user[i]);
-    }
-    const max = Math.max.apply(Math, items.map(function(o) { return o.value; }))
+    // Para saber la puja más alta de la subasta  
+    const marketBid = await Bid.findAll({ 
+        raw: true,
+        where: { marketId: req.params.auctionId },
+    });
+    const items = [ ...marketBid ];
+    const max = Math.max.apply(Math, items.map((o) => o.value));
     const winner = items.find(item => item.value === max);
 
-    let highestBid = {
-        id: null,
-	    value: 0,
-	    buyer: {
-	      id: null,
-	      name: ""
-	    }
-    };
-    if (!(typeof winner === 'undefined' || winner == null)) {
-        highestBid.id = winner.id;
-        highestBid.value = winner.value;
-        highestBid.buyer.id = winner.user.id;
-        highestBid.buyer.name = winner.user.name;
+    const user = await User.findOne({
+        raw: true,
+        where: {
+            id: winner.userId
+        }
+    });
+
+    let highestBid = null;
+    if (winner) {
+        highestBid = {
+            ...winner,
+            id: winner.id,
+            value: winner.value,
+            buyer: {
+                id: user.id, //<---- el user es el que no enccuentra
+                name: user.name //<--- el user es el que no encuentra
+
+            }
+        };
     }
 
-    //Para saber la última puja del usuario
+    // Para saber la última puja del usuario
     const userBid = await Bid.findOne({
-        raw:true,
+        raw: true,
         where: {
             [Op.and]:[{
-                marketId : req.params.marketId
+                marketId : req.params.auctionId
             },
             {
                 userId : req.user.id.id
@@ -102,24 +165,25 @@ router.get('/:marketId', async (req,res) =>{
         }
     });
 
-    let myLastBid = {
-        id: null,
-        value: 0
-    };
-    if (!(typeof userBid === 'undefined' || userBid == null)) {
-        myLastBid.id = userBid.id;
-        myLastBid.value = userBid.value;
+    let myLastBid = null;
+    if (userBid) {
+        myLastBid = {
+            id: userBid.id,
+            value: userBid.value,
+        };
     }
-    
-    const item = JSON.parse(JSON.stringify(market));
-    item.highestBid = highestBid;
-    item.myLastBid = myLastBid;
+
     return res.status(200).json({
         success: true,
-        message:"Subasta recuperada con éxito",
-        item
+        message: "Subasta recuperada con éxito",
+        item: {
+            ...market,
+            highestBid,
+            myLastBid
+        }
     });
-  });
+});
+
   
 router.post('/add', poster);
 
