85e76930893529f7954f7bb1ee5cdc2138b507e1
Daikone <eduardoarzolay@gmail.com>
2023-01-14 19:45:16 -0400

    Añadido un try catch para manejar errores que se puedan dar (muy recomendado mejorarlo, manejo de errores muy simplón)

--- routes/api/gamesPrizes.js
+++ routes/api/gamesPrizes.js
@@ -5,8 +5,12 @@ const responses = require('../../utils/responses/responses');
 const router = Router();
 
 router.get('/', async (req, res) => {
-  await prizeAwarder.award();
-  responses.successDTOResponse(res, 200, 'Se ha recompensado a los usuarios con exito');
+  try {
+    await prizeAwarder.award();
+    return responses.successDTOResponse(res, 200, 'Se ha recompensado a los usuarios con exito');
+  } catch(error) {
+    return responses.errorDTOResponse(res, 500, 'Sucedio un error durante la reparticion de recompensas');
+  }
 });
 
 module.exports = router;
